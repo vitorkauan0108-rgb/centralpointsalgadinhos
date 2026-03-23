0bfe382a49f9a375d5ce31eca68fc0e1666a6165
Vitor Kauan <vitorkauan0108@gmail.com>
2026-03-23 18:31:35 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// Aqui a dona consegue mudar os valores sem tocar na lógica do site
+const CONFIG = {
+  PRECO_POPULAR: 0.6,
+  PRECO_VARIADOS: 21.0,
+  PRECO_CENTO: 36.0,
+  MINIMO_POPULARES: 10,
+  TELEFONE_WHATSAPP: "5521965295921",
+};
